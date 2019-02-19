d8503ee2dce05133ecbf724f523755815d0d74c9
uusa35 <uusa35@gmail.com>
2019-02-19 16:43:07 +0300

    updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,4 +1,4 @@
-const mix = require('laravel-mix');
+const mix = require('laravel-mix').mix;
 
 /*
  |--------------------------------------------------------------------------
@@ -10,8 +10,10 @@ const mix = require('laravel-mix');
  | file for the application as well as bundling up all the JS files.
  |
  */
-mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+mix.js('resources/assets/js/app.js', 'public/js').version()
+    .sass('resources/assets/sass/app.scss', 'public/css').version();
+mix.styles(['resources/assets/css/frontend-custom-ar.css'], 'public/css/frontend-custom-ar.css').version();
+mix.styles(['resources/assets/css/frontend-custom.css'], 'public/css/frontend-custom.css').version();
 // backend
 mix.styles([
     './node_modules/font-awesome/css/font-awesome.min.css',
@@ -20,7 +22,6 @@ mix.styles([
     '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css',
 
     '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css',
-    '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-datetimepicker/css/bootstrap-datetimepicker.min.css',
     '../metronic_v4.5.6/theme/assets/global/plugins/morris/morris.css"',
     '../metronic_v4.5.6/theme/assets/global/plugins/fullcalendar/fullcalendar.min.css"',
     '../metronic_v4.5.6/theme/assets/global/plugins/jqvmap/jqvmap/jqvmap.css"',
@@ -37,21 +38,72 @@ mix.styles([
     '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css',
     '../metronic_v4.5.6/theme/assets/global/plugins/datatables/datatables.min.css',
     '../metronic_v4.5.6/theme/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css',
+    '../metronic_v4.5.6/theme/assets/global/plugins/cubeportfolio/css/cubeportfolio.css',
 
     '../metronic_v4.5.6/theme/assets/global/css/components.min.css',
     '../metronic_v4.5.6/theme/assets/global/css/plugins.min.css',
 
+    '../metronic_v4.5.6/theme/assets/pages/css/portfolio.min.css',
+    '../metronic_v4.5.6/theme/assets/pages/css/profile.min.css',
+    '../metronic_v4.5.6/theme/assets/pages/css/pricing.min.css',
+    '../metronic_v4.5.6/theme/assets/pages/css/blog.min.css',
+
     '../metronic_v4.5.6/theme/assets/layouts/layout2/css/layout.min.css',
     // '../metronic_v4.5.6/theme/assets/layouts/layout/css/themes/darkblue.min.css',
-    // '../metronic_v4.5.6/theme/assets/layouts/layout2/css/themes/blue.min.css',
+    '../metronic_v4.5.6/theme/assets/layouts/layout2/css/themes/blue.min.css',
     // '../metronic_v4.5.6/theme/assets/layouts/layout2/css/themes/light.min.css',
-    '../metronic_v4.5.6/theme/assets/layouts/layout2/css/themes/grey.min.css',
+    // '../metronic_v4.5.6/theme/assets/layouts/layout2/css/themes/grey.min.css',
     '../metronic_v4.5.6/theme/assets/layouts/layout2/css/custom.min.css',
-    './node_modules/jquery.minicolors/jquery.minicolors.css',
-    './resources/css/backend-custom.css'
-], 'public/css/backend.css');
+    './resources/assets/css/backend-custom.css'
+
+], 'public/css/backend.css').version();
+
+mix.styles([
+    './node_modules/font-awesome/css/font-awesome.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/simple-line-icons/simple-line-icons.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/bootstrap/css/bootstrap-rtl.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/bootstrap/css/bootstrap-rtl.min.css',
+
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/bootstrap-daterangepicker/daterangepicker.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/morris/morris.css"',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/fullcalendar/fullcalendar.min.css"',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/jqvmap/jqvmap/jqvmap.css"',
+
+
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/bootstrap-select/css/bootstrap-select-rtl.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/jquery-multi-select/css/multi-select-rtl.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/select2/css/select2.min-rtl.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/select2/css/select2-bootstrap.min.css',
+    '../metronic_v4.5.6/theme_rtl//assets/global/plugins/bootstrap-colorpicker/css/colorpicker.css',
+    '../metronic_v4.5.6/theme_rtl//assets/global/plugins/jquery-minicolors/jquery.minicolors.css',
+
+
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/bootstrap-datepicker/css/bootstrap-datepicker3.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/datatables/datatables.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap-rtl.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/plugins/cubeportfolio/css/cubeportfolio.css',
+
+    '../metronic_v4.5.6/theme_rtl/assets/global/css/components-rtl.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/global/css/plugins-rtl.min.css',
+
+
+    '../metronic_v4.5.6/theme_rtl/assets/pages/css/portfolio-rtl.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/pages/css/profile-rtl.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/pages/css/pricing-rtl.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/pages/css/blog-rtl.min.css',
+
+    '../metronic_v4.5.6/theme_rtl/assets/layouts/layout2/css/layout-rtl.min.css',
+    // '../metronic_v4.5.6/theme_rtl/assets/layouts/layout/css/themes/darkblue-rtl.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/layouts/layout2/css/themes/blue-rtl.min.css',
+    // '../metronic_v4.5.6/theme_rtl/assets/layouts/layout2/css/themes/light-rtl.min.css',
+    // '../metronic_v4.5.6/theme_rtl/assets/layouts/layout2/css/themes/grey.min.css',
+    '../metronic_v4.5.6/theme_rtl/assets/layouts/layout2/css/custom-rtl.min.css',
+    './resources/assets/css/backend-custom.css',
+    'resources/assets/css/backend-custom-ar.css',
+], 'public/css/backend-rtl.css').version();
+
 mix.scripts([
-        '../metronic_v4.5.6/theme/assets/global/plugins/respond.min.js',
+        '../metronic_v4.5.6/theme_rtl/assets/global/plugins/respond.min.js',
         '../metronic_v4.5.6/theme/assets/global/plugins/excanvas.min.js',
         '../metronic_v4.5.6/theme/assets/global/plugins/jquery.min.js',
         '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap/js/bootstrap.min.js',
@@ -69,17 +121,14 @@ mix.scripts([
         '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-switch/js/bootstrap-switch.min.js',
         '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-colorpicker/js/bootstrap-colorpicker.js',
         '../metronic_v4.5.6/theme/assets/global/plugins/jquery-minicolors/jquery.minicolors.min.js',
-
-        '../metronic_v4.5.6/theme/assets/global/plugins/moment.min.js',
-        '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
-        // not needed
-        '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-datepicker/js/bootstrap-datepicker.min.js',
-        '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-timepicker/js/bootstrap-timepicker.min.js',
-        '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-datetimepicker/js/bootstrap-datetimepicker.min.js',
-
+        '../metronic_v4.5.6/theme/assets/global/plugins/cubeportfolio/js/jquery.cubeportfolio.min.js',
         '../metronic_v4.5.6/theme/assets/global/scripts/app.min.js',
-        '../metronic_v4.5.6/theme/assets/pages/scripts/components-date-time-pickers.min.js',
 
+        '../metronic_v4.5.6/theme/assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.js',
+        '../metronic_v4.5.6/theme/assets/global/plugins/jquery.sparkline.min.js',
+        '../metronic_v4.5.6/theme/assets/pages/scripts/profile.min.js',
+        '../metronic_v4.5.6/theme/assets/pages/scripts/portfolio-4.min.js',
+        '../metronic_v4.5.6/theme/assets/pages/scripts/portfolio-1.min.js',
 
         '../metronic_v4.5.6/theme/assets/pages/scripts/components-multi-select.min.js',
         '../metronic_v4.5.6/theme/assets/pages/scripts/components-color-pickers.min.js',
@@ -87,105 +136,17 @@ mix.scripts([
         '../metronic_v4.5.6/theme/assets/layouts/layout2/scripts/demo.min.js',
         '../metronic_v4.5.6/theme/assets/layouts/global/scripts/quick-sidebar.min.js',
     ],
-    'public/js/backend.js');
-mix.scripts('./node_modules/tinymce/tinymce.min.js', 'public/js/tinymce.min.js');
-mix.scripts('resources/js/backend-custom.js', 'public/js/backend-custom.js');
-mix.styles([
-        '../bella/bella-files/bella/assets/plugins/bootstrap/css/bootstrap.min.css',
-        '../bella/bella-files/bella/assets/plugins/bootstrap-select/css/bootstrap-select.min.css',
-        '../bella/bella-files/bella/assets/plugins/fontawesome/css/font-awesome.min.css',
-        '../bella/bella-files/bella/assets/plugins/prettyphoto/css/prettyPhoto.css',
-        '../bella/bella-files/bella/assets/plugins/owl-carousel2/assets/owl.carousel.min.css',
-        '../bella/bella-files/bella/assets/plugins/owl-carousel2/assets/owl.theme.default.min.css',
-        '../bella/bella-files/bella/assets/plugins/animate/animate.min.css',
-        '../bella/bella-files/bella/assets/plugins/countdown/jquery.countdown.css',
-        '../bella/bella-files/bella/assets/css/theme.css',
-        'resources/css/theme.css',
-        'resources/css/frontend-custom.css',
-        'resources/css/frontend-custom-en.css',
-    ]
-    , 'public/css/frontend.css');
-mix.styles([
-    '../bella/bella-files/bella-rtl/assets/plugins/bootstrap/css/bootstrap.min.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/bootstrap/css/bootstrap-rtl.min.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/bootstrap-select/css/bootstrap-select.min.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/fontawesome/css/font-awesome.min.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/prettyphoto/css/prettyPhoto.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/owl-carousel2/assets/owl.carousel.min.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/owl-carousel2/assets/owl.theme.default.min.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/animate/animate.min.css',
-    '../bella/bella-files/bella-rtl/assets/plugins/countdown/jquery.countdown.css',
-    '../bella/bella-files/bella-rtl/assets/css/theme.css',
-    // '../bella/bella-files/bella-rtl/assets/css/theme-gold.css',
-    'resources/css/theme.css',
-    'resources/css/frontend-custom.css',
-    'resources/css/frontend-custom-ar.css',
-    'resources/js/cartIndex.js'
-], 'public/css/frontend-rtl.css');
-// mix.styles('resources/css/order-review.css','public/css/order-review.css');
-mix.babel([
-        '../bella/bella-files/bella/assets/plugins/modernizr.custom.js',
-        '../bella/bella-files/bella/assets/plugins/iesupport/html5shiv.js',
-        '../bella/bella-files/bella/assets/plugins/iesupport/respond.min.js',
-        '../bella/bella-files/bella/assets/plugins/jquery/jquery-1.11.1.min.js',
-        '../bella/bella-files/bella/assets/plugins/bootstrap/js/bootstrap.min.js',
-        '../bella/bella-files/bella/assets/plugins/bootstrap-select/js/bootstrap-select.min.js',
-        // '../bella/bella-files/bella/assets/plugins/prettyphoto/js/jquery.prettyPhoto.js',
-        '../bella/bella-files/bella/assets/plugins/superfish/js/superfish.min.js',
-        '../bella/bella-files/bella/assets/plugins/owl-carousel2/owl.carousel.min.js',
-        '../bella/bella-files/bella/assets/plugins/jquery.sticky.min.js',
-        '../bella/bella-files/bella/assets/plugins/jquery.easing.min.js',
-        '../bella/bella-files/bella/assets/plugins/jquery.smoothscroll.min.js',
-        '../bella/bella-files/bella/assets/plugins/smooth-scrollbar.min.js',
-        '../bella/bella-files/bella/assets/plugins/countdown/jquery.plugin.min.js',
-        '../bella/bella-files/bella/assets/plugins/countdown/jquery.countdown.min.js',
-        '../bella/bella-files/bella/assets/js/theme.js',
-        '../bella/bella-files/bella/assets/plugins/jquery.cookie.js',
-        'resources/js/frontend-custom.js',
-        'resources/js/frontend-custom-en.js',
-        'resources/js/cartIndex.js'
-    ]
-    , 'public/js/frontend.js');
+    'public/js/backend.js').version();
 
-mix.babel([
-        '../bella/bella-files/bella-rtl/assets/plugins/modernizr.custom.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/iesupport/html5shiv.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/iesupport/respond.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/jquery/jquery-1.11.1.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/bootstrap/js/bootstrap.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/bootstrap-select/js/bootstrap-select.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/superfish/js/superfish.min.js',
-        // '../bella/bella-files/bella-rtl/assets/plugins/prettyphoto/js/jquery.prettyPhoto.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/owl-carousel2/owl.carousel.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/jquery.sticky.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/jquery.easing.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/jquery.smoothscroll.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/smooth-scrollbar.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/countdown/jquery.plugin.min.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/countdown/jquery.countdown.min.js',
-        '../bella/bella-files/bella-rtl/assets/js/theme.js',
-        '../bella/bella-files/bella-rtl/assets/plugins/jquery.cookie.js',
-        'resources/js/frontend-custom.js',
-        'resources/js/frontend-custom-ar.js',
-        'resources/js/cartIndex.js'
-    ]
-    , 'public/js/frontend-rtl.js');
-mix.copy('resources/js/jquery.prettyPhoto.min.js', 'public/js/jquery.prettyPhoto.min.js');
-mix.css('resources/css/order-review.css', 'public/css/order-review');
+// mix.scripts('./node_modules/tinymce/tinymce.min.js', 'public/js/tinymce.min.js').version();
+mix.scripts('resources/assets/js/backend-custom.js', 'public/js/backend-custom.js');
 mix.copyDirectory('../metronic_v4.5.6/theme/assets/global/plugins/simple-line-icons/fonts', 'public/css/fonts');
-mix.copyDirectory('..//metronic_v4.5.6/theme/assets/global/plugins/datatables/images', 'public/plugins/datatables/images');
-mix.copyDirectory('../metronic_v4.5.6/theme/assets/global/img', 'public/img');
-mix.copyDirectory('../metronic_v4.5.6/theme/assets/layouts/layout/img', 'public/img');
+mix.copyDirectory('./../metronic_v4.5.6/theme/assets/global/plugins/datatables/images', 'public/plugins/datatables/images');
+mix.copyDirectory('./../metronic_v4.5.6/theme/assets/global/plugins/cubeportfolio/img', 'public/img');
+mix.copyDirectory('./../metronic_v4.5.6/theme/assets/layouts/layout2/img', 'public/img');
+mix.copyDirectory('./../metronic_v4.5.6/theme//assets/global/img/flags/', 'public/img/flags');
 mix.copyDirectory('./node_modules/font-awesome/fonts', 'public/fonts');
 mix.copyDirectory('./node_modules/tinymce/plugins', 'public/js/plugins');
 mix.copyDirectory('./node_modules/tinymce/skins', 'public/js/skins');
 mix.copyDirectory('./node_modules/tinymce/themes', 'public/js/themes');
-mix.copyDirectory('../bella/bella-files/bella/assets/img', 'public/img');
-mix.copyDirectory('../bella/bella-files/bella/assets/ico', 'public/ico');
-mix.copyDirectory('../bella/bella-files/bella/assets/plugins/prettyphoto/images', 'public/images');
-if (mix.inProduction()) {
-    mix.version();
-}
-if(!mix.inProduction) {
-    mix.sourceMaps();
-}
+
