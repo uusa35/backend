e9c42f8b3e300e872c4755513275a448587eb6cb
uusa35 <uusa35@gmail.com>
2019-02-27 16:56:28 +0300

    working on wokiee theme

--- resources/views/frontend/wokiee/four/home.blade.php
+++ resources/views/frontend/wokiee/four/home.blade.php
@@ -11,7 +11,7 @@
 {{--    @include('frontend.wokiee.four.partials._five_categories')--}}
 {{--    @include('frontend.wokiee.four.partials._all_children_for_category')--}}
 {{--    @include('frontend.wokiee.four.partials._all_slider_products_for_one_parent_category')--}}
-    @include('frontend.wokiee.four.partials._all_brands')
+{{--    @include('frontend.wokiee.four.partials._all_brands')--}}
     @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.new_arrivals'), 'elements' => $newArrivals])
     @include('frontend.wokiee.four.partials._ads_top')
     @include('frontend.wokiee.four.partials._products_slider_hot_deal')
