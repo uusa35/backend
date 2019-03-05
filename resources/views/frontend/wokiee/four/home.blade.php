9bd9fa9314b1202417ff31b7f5b624644131b52f
uusa35 <uusa35@gmail.com>
2019-03-05 19:54:09 +0300

    updates

--- resources/views/frontend/wokiee/four/home.blade.php
+++ resources/views/frontend/wokiee/four/home.blade.php
@@ -1,16 +1,16 @@
 @extends('frontend.wokiee.four.layouts.app')
 
 @section('body')
-    @if(request()->has('mallr') && !app()->environment('development'))
+    @if(request()->has('mallr'))
         @include('frontend.wokiee.four.partials.slider')
     @endif
-    @if(request()->has('evento') && !app()->environment('development'))
+    @if(request()->has('evento'))
         @include('frontend.wokiee.four.partials._btns_home')
         @include('frontend.wokiee.four.partials._commercials_top', ['elements' => $topDoubleCommercials])
         @include('frontend.wokiee.four.partials._services_slider_hot_deal', ['elements' => $serviceHotDeals])
         @include('frontend.wokiee.four.partials._commercials_top', ['elements' => $bottomDoubleCommercials])
     @endif
-    @if(request()->has('mallr') && !app()->environment('development'))
+    @if(request()->has('mallr'))
         @include('frontend.wokiee.four.partials._five_categories')
         @include('frontend.wokiee.four.partials._all_brands')
         @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.new_arrivals'), 'elements' => $newProducts])
