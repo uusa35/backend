b2f77df3c15dcae3a9bd69decf7b20306d2ba438
uusa35 <uusa35@gmail.com>
2019-03-11 11:40:40 +0300

    updates

--- database/factories/ShipmentPackageFactory.php
+++ database/factories/ShipmentPackageFactory.php
@@ -12,7 +12,6 @@ $factory->define(ShipmentPackage::class, function (Faker $faker) {
         'slug_en' => $faker->name,
         'charge' => $faker->randomFloat(1,0,9),
         'active' => $faker->boolean(true),
-        'is_local' => $faker->boolean(),
         'user_id' => User::companies()->get()->random()->id,
         'country_id' => Country::active()->get()->random()->id,
     ];
