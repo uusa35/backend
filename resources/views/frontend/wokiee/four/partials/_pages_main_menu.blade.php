fce4043450aebe12c0b9e97aac2ab80ba8e2eb5e
uusa35 <uusa35@gmail.com>
2019-03-17 12:38:46 +0300

    updautes

--- resources/views/frontend/wokiee/four/partials/_pages_main_menu.blade.php
+++ resources/views/frontend/wokiee/four/partials/_pages_main_menu.blade.php
@@ -2,7 +2,7 @@
     @foreach($pages->where('on_menu_desktop',true)->take(3) as $page)
         <li class="dropdown">
             <a href="{{ route('frontend.page.show.name',['id' => $page->id,'name' => $page->title]) }}">
-                {{ str_limit($page->title,11,'') }}
+                {{ str_limit($page->title,5,'') }}
             </a>
         </li>
     @endforeach
