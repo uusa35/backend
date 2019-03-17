dc90a4b7c0280c033579645eba2a7ba1e46e6ac8
uusa35 <uusa35@gmail.com>
2019-03-17 13:32:01 +0300

    updautes

--- resources/views/frontend/wokiee/four/partials/_service_show_gallery_mobile.blade.php
+++ resources/views/frontend/wokiee/four/partials/_service_show_gallery_mobile.blade.php
@@ -1,13 +1,23 @@
 <div class="tt-mobile-product-layout visible-xs">
     <div class="tt-mobile-product-slider arrow-location-center slick-animated-show-js">
-        <div><img src="{{ $element->imageLinkLarge }}" alt=""></div>
-        @foreach($element->images as $img)
-            <div><img src="{{ $img->imageLargeLink}}" alt=""></div>
-        @endforeach
+        <div class="slick-slide slick-current slick-active"><img class="active" src="{{ $element->imageLargeLink }}" alt="{{ $element->slug }}"></div>
+        @if($element->images->isNotEmpty())
+            @foreach($element->images as $img)
+                <div><img src="{{ $img->imageLargeLink}}" alt="{{ $element->slug }}"></div>
+            @endforeach
+        @endif
         @if($element->video_url)
             <div>
                 <div class="embed-responsive embed-responsive-16by9">
                     <iframe class="embed-responsive-item" src="{{ $element->video_url }}"
+                            allowfullscreen></iframe>
+                </div>
+            </div>
+        @endif
+        @if($element->video_url_two)
+            <div>
+                <div class="embed-responsive embed-responsive-16by9">
+                    <iframe class="embed-responsive-item" src="{{ $element->video_url_two }}"
                             allowfullscreen></iframe>
                 </div>
             </div>
