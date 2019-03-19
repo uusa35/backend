1d0cd6839576b58f3291662670ce086c391907bc
uusa35 <uusa35@gmail.com>
2019-03-19 13:52:31 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/styles.blade.php
+++ resources/views/frontend/wokiee/four/partials/styles.blade.php
@@ -4,8 +4,8 @@
     .tt-color-scheme-02,
     .tt-back-to-top,
     /*.tt-dropdown-toggle,*/
-    .tt-badge-cart,
-    .tt-show, .tt-search-type2-rtl, .tt-btn-search-rtl
+    .tt-badge-cart, .tt-product:not(.tt-view) .tt-image-box .tt-btn-quickview:hover,
+    .tt-show, .tt-search-type2-rtl, .tt-btn-search-rtl, .tt-promo-box.hover-type-2:hover .tt-description-wrapper .tt-background
     {
         background-color: {{ $settings->main_theme_color }} !important;
     }
@@ -20,7 +20,7 @@
      .tt-price, .old-price,
     .money,
     header .tt-multi-obj ul li.active a, header .tt-multi-obj ul li.active a [class^=icon-], header .tt-multi-obj ul li a:hover, header .tt-multi-obj ul li a:hover [class^=icon-],
-    footer .tt-color-scheme-01, .tt-table-shop-01 tbody td a, .product-information-buttons a
+    footer .tt-color-scheme-01, .tt-table-shop-01 tbody td a, .product-information-buttons a, .tt-services-block .tt-col-icon
     {
         color : {{ $settings->title_theme_color }} !important;
     }
@@ -28,7 +28,7 @@
     .tt-collapse-title {
         border-bottom: 1px solid {{ $settings->main_theme_color }};
     }
-    .btn.btn-border {
+    .btn.btn-border, .tt-btn-quickview:hover, .btn.btn-small:hover {
         color : white !important;
         border : none !important;
     }
