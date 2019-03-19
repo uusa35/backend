5c0b7aa851be38fbc63f63773ee3bba0090ad98b
uusa35 <uusa35@gmail.com>
2019-03-19 10:31:43 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_categories_main_menu_element_with_images.blade.php
+++ resources/views/frontend/wokiee/four/partials/_categories_main_menu_element_with_images.blade.php
@@ -1,8 +1,76 @@
 @if(isset($categories) && $categories->isNotEmpty())
     @if($categories->isNotEmpty())
-        @foreach($categories->where('is_parent', true) as $cat)
+        @if(env('APP_CASE') === 'evento')
+            @foreach($categories->where('is_parent', true) as $cat)
+                <li class="dropdown megamenu">
+                    <a href="{{ route('frontend.service.search',['service_category_id' => $cat->id]) }}">{{ str_limit($cat->name,12,'') }}</a>
+                    @if($cat->children->isNotEmpty())
+                        <div class="dropdown-menu">
+                            <div class="row">
+                                <div class="col-sm-9">
+                                    <div class="row tt-col-list">
+                                        @foreach($cat->children as $sub)
+                                            <div class="col-sm-4">
+                                                <a href="{{ route('frontend.service.search',['service_category_id' => $sub->id]) }}"
+                                                   class="tt-title-submenu">
+                                                    @if($sub->imageLargeLink)
+                                                        {{ $sub->name }}
+                                                        <img class="img-menu-category img-responsive"
+                                                             src="{{ $sub->imageLargeLink }}"
+                                                             data-src="{{ $sub->imageLargeLink }}"
+                                                             alt="{{ $sub->name }}">
+                                                    @endif
+                                                </a>
+                                                @if($sub->children->isNotEmpty())
+                                                    <ul class="tt-megamenu-submenu">
+                                                        @foreach($sub->children as $child)
+                                                            <li>
+                                                                <a href="{{ route('frontend.service.search',['service_category_id' => $child->id]) }}">{{ $child->name }}
+                                                                    @if($child->on_new)
+                                                                        <span class="tt-badge tt-new">{{ trans('general.new') }}</span>
+                                                                    @endif
+                                                                </a>
+                                                            </li>
+                                                        @endforeach
+                                                    </ul>
+                                                @endif
+                                            </div>
+                                        @endforeach
+                                    </div>
+                                </div>
+                                @if($cat->imageLargeLink)
+                                    <div class="col-sm-3">
+                                        <div class="tt-offset-7">
+                                            <a href="{{ route('frontend.service.search',['service_category_id' => $cat->id]) }}"
+                                               class="tt-promo-02">
+                                                <img class="img-category img-responsive"
+                                                     src="{{ $cat->imageLargeLink }}"
+                                                     data-src="{{ $cat->imageLargeLink }}"
+                                                     alt="{{ $cat->name }}">
+                                                <div class="tt-description tt-point-h-l tt-point-v-t">
+                                                    <div class="tt-description-wrapper">
+                                                        <div class="tt-title-small tt-white-color">{{ $cat->name }}</div>
+                                                        @if($cat->caption)
+                                                            <div class="tt-title-xlarge tt-white-color">{{ $cat->caption }}</div>
+                                                        @endif
+                                                        @if($cat->description)
+                                                            <p class="tt-white-color">{{ $cat->description }}</p>
+                                                            <span class="btn-underline tt-obj-bottom tt-white-color">{{ trans('general.shop_now') }}</span>
+                                                        @endif
+                                                    </div>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                @endif
+                            </div>
+                        </div>
+                    @endif
+                </li>
+            @endforeach
+        @else
             <li class="dropdown megamenu">
-                <a href="{{ route('frontend.service.search',['service_category_id' => $cat->id]) }}">{{ str_limit($cat->name,12,'') }}</a>
+                <a href="{{ route('frontend.product.search',['product_category_id' => $cat->id]) }}">{{ str_limit($cat->name,12,'') }}</a>
                 @if($cat->children->isNotEmpty())
                     <div class="dropdown-menu">
                         <div class="row">
@@ -10,7 +78,8 @@
                                 <div class="row tt-col-list">
                                     @foreach($cat->children as $sub)
                                         <div class="col-sm-4">
-                                            <a href="{{ route('frontend.service.search',['service_category_id' => $sub->id]) }}" class="tt-title-submenu">
+                                            <a href="{{ route('frontend.product.search',['product_category_id' => $sub->id]) }}"
+                                               class="tt-title-submenu">
                                                 @if($sub->imageLargeLink)
                                                     {{ $sub->name }}
                                                     <img class="img-menu-category img-responsive"
@@ -23,7 +92,7 @@
                                                 <ul class="tt-megamenu-submenu">
                                                     @foreach($sub->children as $child)
                                                         <li>
-                                                            <a href="{{ route('frontend.service.search',['service_category_id' => $child->id]) }}">{{ $child->name }}
+                                                            <a href="{{ route('frontend.product.search',['product_category_id' => $child->id]) }}">{{ $child->name }}
                                                                 @if($child->on_new)
                                                                     <span class="tt-badge tt-new">{{ trans('general.new') }}</span>
                                                                 @endif
@@ -39,7 +108,8 @@
                             @if($cat->imageLargeLink)
                                 <div class="col-sm-3">
                                     <div class="tt-offset-7">
-                                        <a href="{{ route('frontend.service.search',['service_category_id' => $cat->id]) }}" class="tt-promo-02">
+                                        <a href="{{ route('frontend.product.search',['product_category_id' => $cat->id]) }}"
+                                           class="tt-promo-02">
                                             <img class="img-category img-responsive" src="{{ $cat->imageLargeLink }}"
                                                  data-src="{{ $cat->imageLargeLink }}"
                                                  alt="{{ $cat->name }}">
@@ -63,6 +133,6 @@
                     </div>
                 @endif
             </li>
-        @endforeach
+        @endif
     @endif
 @endif
