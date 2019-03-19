e5c4007e28f802ce243e87fc7ce1f8dd07f83492
uusa35 <uusa35@gmail.com>
2019-03-19 10:44:42 +0300

    updates

--- app/Services/Helpers.php
+++ app/Services/Helpers.php
@@ -135,7 +135,7 @@ function getClientCountry()
     $kwt = '188.70.48.243';
     $ksa = '176.17.238.199';
     $uae = '109.177.176.229';
-    $user_ip = app()->isLocal() ? $kwt : get_client_ip();
+    $user_ip = app()->isLocal() ? $ksa : get_client_ip();
     $geo = unserialize(file_get_contents("http://www.geoplugin.net/php.gp?ip=$user_ip"));
     $country = $geo["geoplugin_countryName"];
     $city = $geo["geoplugin_city"];
