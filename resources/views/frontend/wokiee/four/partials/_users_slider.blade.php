2bb2bfd08e23cac34b9913780b3501d46e8f0806
uusa35 <uusa35@gmail.com>
2019-03-20 13:43:42 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_users_slider.blade.php
+++ resources/views/frontend/wokiee/four/partials/_users_slider.blade.php
@@ -2,8 +2,8 @@
     <div class="container container-fluid-custom-mobile-padding">
         @if($title)
             <div class="tt-block-title">
-                <h1 class="tt-title">{{ trans('general.recent_designers') }}</h1>
-                <div class="tt-description">{{ trans('message.recent_designers') }}</div>
+                <h1 class="tt-title">{{ trans('general.our_designers') }}</h1>
+                <div class="tt-description">{{ trans('message.our_designers') }}</div>
             </div>
         @endif
         <div class="tt-carousel-products row arrow-location-tab arrow-location-tab01 tt-alignment-img tt-layout-product-item slick-animated-show-js"
