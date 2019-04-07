5fd4fb1b153b9d70fb66659049b399c99cf5d9f5
uusa35 <uusa35@gmail.com>
2019-04-07 15:45:21 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_products_user_show.blade.php
+++ resources/views/frontend/wokiee/four/partials/_products_user_show.blade.php
@@ -1,7 +1,7 @@
 @if(isset($products) && $products->isNotEmpty())
     <div class="row">
         @foreach($products as $product)
-            <div class="col-lg-4 col-md-6 tt-col-item">
+            <div class="col-lg-4 col-md-6">
                 @include('frontend.wokiee.four.partials._product_widget',['element' => $product])
             </div>
         @endforeach
--- storage/framework/views/5a1c271e7ba248d36dd89c28391ff74e74b4103c.php
+++ storage/framework/views/5a1c271e7ba248d36dd89c28391ff74e74b4103c.php
@@ -1,7 +1,7 @@
 <?php if(isset($products) && $products->isNotEmpty()): ?>
     <div class="row">
         <?php $__currentLoopData = $products; $__env->addLoop($__currentLoopData); foreach($__currentLoopData as $product): $__env->incrementLoopIndices(); $loop = $__env->getLastLoop(); ?>
-            <div class="col-lg-4 col-md-6 tt-col-item">
+            <div class="col-lg-4 col-md-6">
                 <?php echo $__env->make('frontend.wokiee.four.partials._product_widget',['element' => $product], \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
             </div>
         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
