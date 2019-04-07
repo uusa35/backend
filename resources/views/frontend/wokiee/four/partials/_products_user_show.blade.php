46afae0be7df5c499a5562a3a66e8bfcac135477
uusa35 <uusa35@gmail.com>
2019-04-07 15:48:59 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/_products_user_show.blade.php
+++ resources/views/frontend/wokiee/four/partials/_products_user_show.blade.php
@@ -1,7 +1,7 @@
 @if(isset($products) && $products->isNotEmpty())
-    <div class="row">
+    <div class="col-lg-12">
         @foreach($products as $product)
-            <div class="col-lg-4 col-md-6 tt-col-item">
+            <div class="col-lg-4 col-md-6">
                 @include('frontend.wokiee.four.partials._product_widget',['element' => $product])
             </div>
         @endforeach
