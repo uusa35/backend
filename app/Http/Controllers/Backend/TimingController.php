e7988c6592e2ce9f10152647421368f8939595ce
uusa35 <uusa35@gmail.com>
2019-04-18 09:13:20 +0300

    timing controller

---
+++ app/Http/Controllers/Backend/TimingController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers\Backend;
+
+use App\Models\Day;
+use App\Models\Service;
+use App\Models\Timing;
+use App\Models\User;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class TimingController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $elements = Timing::whereHas('service', function ($q) {
+            !request()->has('timing_id') ? $q->where('user_id', auth()->id()) : $q->where(['user_id' => auth()->id(), 'id' => request('id')]);
+        })->get();
+        return view('backend.modules.timing.index', compact('elements'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $services = auth()->user()->isAdminOrAbove ? Service::active()->get() : auth()->user()->services()->get();
+        $users = User::active()->get();
+        $days = Day::all();
+        return view('backend.modules.timing.create', compact('services', 'users', 'days'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
