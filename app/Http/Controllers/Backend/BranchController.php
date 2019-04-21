d546ddd8f44bd6b39b1a3d76aaefd8bb94918e88
uusa35 <uusa35@gmail.com>
2019-04-21 13:36:50 +0300

    done with settings

--- app/Http/Controllers/Backend/BranchController.php
+++ app/Http/Controllers/Backend/BranchController.php
@@ -15,6 +15,7 @@ class BranchController extends Controller
      */
     public function index()
     {
+
         $elements = Branch::latest()->get();
         return view('backend.modules.branch.index', compact('elements'));
     }
