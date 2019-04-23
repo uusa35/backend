e0f036a73b25e885cae65bcf58ff7730355d0615
uusa35 <uusa35@gmail.com>
2019-04-23 11:26:33 +0300

    more updates to policies and trans

--- storage/framework/views/426f2f05204c36bc2d4bda9529084c71d3247597.php
+++ storage/framework/views/426f2f05204c36bc2d4bda9529084c71d3247597.php
@@ -1,3 +1,7 @@
+<?php $__env->startSection('breadcrumbs'); ?>
+    <?php echo e(Breadcrumbs::render('backend.admin.notification.index')); ?>
+
+<?php $__env->stopSection(); ?>
 <?php $__env->startSection('content'); ?>
     <div class="row">
         <div class="col-md-12">
@@ -5,32 +9,25 @@
             <div class="portlet light ">
                 <?php echo $__env->make('backend.partials.forms.form_title', \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
                 <div class="portlet-body">
-                    <div class="m-heading-1 border-green m-bordered">
-                        <h3>Important Information</h3>
-                        <p>
-                            Roles are very important for the application.
-                        </p>
-                        <p> Some Information about roles.
-                            <a class="btn red btn-outline" href="http://datatables.net/" target="_blank">the official
-                                documentation</a>
-                        </p>
-                    </div>
+                    <?php echo $__env->make('backend.partials._admin_instructions',['title' => trans('general.notifications') ,'message' => trans('message.index_notification')], \Illuminate\Support\Arr::except(get_defined_vars(), array('__data', '__path')))->render(); ?>
                     <table id="dataTable" class="table table-striped table-bordered table-hover" cellspacing="0">
                         <thead>
                         <tr>
-                            <th>id</th>
-                            <th>title</th>
-                            <th>description</th>
-                            <th>sent at</th>
+                            <th><?php echo e(trans('general.id')); ?></th>
+                            <th><?php echo e(trans('general.title')); ?></th>
+                            <th><?php echo e(trans('general.type')); ?></th>
+                            <th><?php echo e(trans('general.description')); ?></th>
+                            <th><?php echo e(trans('general.sent_at')); ?></th>
                             
                         </tr>
                         </thead>
                         <tfoot>
                         <tr>
-                            <th>id</th>
-                            <th>title</th>
-                            <th>description</th>
-                            <th>sent at</th>
+                            <th><?php echo e(trans('general.id')); ?></th>
+                            <th><?php echo e(trans('general.title')); ?></th>
+                            <th><?php echo e(trans('general.type')); ?></th>
+                            <th><?php echo e(trans('general.description')); ?></th>
+                            <th><?php echo e(trans('general.sent_at')); ?></th>
                             
                         </tr>
                         </tfoot>
@@ -39,37 +36,38 @@
                             <tr>
                                 <td> <?php echo e($element->id); ?></td>
                                 <td> <?php echo e($element->title); ?> </td>
+                                <td> <?php echo e($element->type); ?> </td>
                                 <td> <?php echo e($element->description); ?> </td>
-                                <td> <?php echo e($element->created_at); ?> </td>
+                                <td> <?php echo e($element->created_at->diffForHumans()); ?> </td>
                                 
-                                    
-                                        
-                                                
-                                            
-                                        
-                                        
-                                            
-                                                
-                                                    
-                                            
-                                            
-                                                
-                                                   
-                                                   
-                                                   
-                                                
-                                                    
-                                                
-                                                      
-                                                    
-                                                    
-                                                    
-                                                        
-                                                    
-                                                
-                                            
-                                        
-                                    
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
+                                
                                 
                             </tr>
                         <?php endforeach; $__env->popLoop(); $loop = $__env->getLastLoop(); ?>
