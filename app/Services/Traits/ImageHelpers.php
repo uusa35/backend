32bef570fdb27e8787cded7071bb0f8d6f4fdd1f
uusa35 <uusa35@gmail.com>
2021-08-30 08:48:56 +0300

    updates

--- app/Services/Traits/ImageHelpers.php
+++ app/Services/Traits/ImageHelpers.php
@@ -86,8 +86,7 @@ trait ImageHelpers
                                         } else {
                                             $img->resize($dimensions[0], $dimensions[1]);
                                         }
-                                        dd(env('IMAGE_QUALITY'));
-                                        $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                                        $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
                                     } elseif ($value === 'medium') {
                                         if ($ratio) {
                                             $img->resize($dimensions[0] / 2, null, function ($constraint) {
@@ -96,7 +95,7 @@ trait ImageHelpers
                                         } else {
                                             $img->resize($dimensions[0] / 2, $dimensions[0] / 2);
                                         }
-                                        $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                                        $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
                                     } elseif ($value === 'thumbnail') {
                                         if ($ratio) {
                                             $img->resize($dimensions[0] / 3, null, function ($constraint) {
@@ -105,7 +104,7 @@ trait ImageHelpers
                                         } else {
                                             $img->resize($dimensions[0] / 3, $dimensions[0] / 3);
                                         }
-                                        $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                                        $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
                                     }
                                 }
                                 $model->update([
@@ -273,7 +272,7 @@ trait ImageHelpers
                 } else {
                     $img->resize($dimensions[0], $dimensions[1]);
                 }
-                $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
             } elseif ($value === 'medium') {
                 if ($ratio) {
                     $img->resize($dimensions[0] / 2, null, function ($constraint) {
@@ -282,7 +281,7 @@ trait ImageHelpers
                 } else {
                     $img->resize($dimensions[0] / 2, $dimensions[0] / 2);
                 }
-                $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
             } elseif ($value === 'thumbnail') {
                 if ($ratio) {
                     $img->resize($dimensions[0] / 3, null, function ($constraint) {
@@ -292,7 +291,7 @@ trait ImageHelpers
                     $img->resize($dimensions[0] / 3, $dimensions[0] / 3);
                 }
 //                $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath));
-                $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
             }
         }
         Storage::delete(public_path('storage/uploads/images/' . $imagePath));
@@ -331,7 +330,7 @@ trait ImageHelpers
                             } else {
                                 $img->resize($dimensions[0], $dimensions[1]);
                             }
-                            $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                            $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
                         } elseif ($value === 'medium') {
                             if ($ratio) {
                                 $img->resize($dimensions[0], null, function ($constraint) {
@@ -340,7 +339,7 @@ trait ImageHelpers
                             } else {
                                 $img->resize($dimensions[0] / 2, $dimensions[0] / 2);
                             }
-                            $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                            $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
                         }
                         if ($value === 'thumbnail') {
                             if ($ratio) {
@@ -350,7 +349,7 @@ trait ImageHelpers
                             } else {
                                 $img->resize($dimensions[0] / 3, $dimensions[0] / 3);
                             }
-                            $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), env('IMAGE_QUALITY'));
+                            $img->save(storage_path('app/public/uploads/images/' . $value . '/' . $imagePath), (int) env('IMAGE_QUALITY'));
                         }
                     }
                     $newImage = \App\Models\Image::create(['user_id' => $model->id, 'name' => $imagePath]);
