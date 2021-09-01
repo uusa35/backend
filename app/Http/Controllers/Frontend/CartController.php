f6905d6c35b8e4b2e668e816acbdcb8f8de40c42
uusa35 <uusa35@gmail.com>
2021-09-01 21:11:34 +0300

    updates

--- app/Http/Controllers/Frontend/CartController.php
+++ app/Http/Controllers/Frontend/CartController.php
@@ -88,7 +88,6 @@ class CartController extends Controller
             }
             return redirect()->back()->with('error', trans('message.product_is_not_added_to_cart_successfully'));
         } catch (\Exception $e) {
-            dd($e->getMessage());
             return redirect()->back()->with('error',$e->getMessage());
         }
     }
