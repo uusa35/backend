88667c71a95512ea18b7e327ffa1fc5b81b9984f
uusa35 <uusa35@gmail.com>
2021-12-21 08:40:51 +0300

    updates

--- resources/views/backend/partials/_sidebar_super.blade.php
+++ resources/views/backend/partials/_sidebar_super.blade.php
@@ -491,7 +491,7 @@
             </a>
             <ul class="sub-menu">
                 <li class="nav-item ">
-                    <a href="{{ route('backend.admin.order.search',['cash_on_delivery' => true]) }}"
+                    <a href="{{ route('backend.admin.order.search',['cash_on_delivery' => true, 'paid' => true]) }}"
                        class="nav-link nav-toggle">
                         <i class="fa fa-fw fa-pie-chart"></i>
                         <span class="title">{{ trans('general.cash_on_delivery') }}</span>
@@ -499,7 +499,7 @@
                     </a>
                 </li>
                 <li class="nav-item ">
-                    <a href="{{ route('backend.admin.order.search',['paid' => true, 'payment_method' => true]) }}"
+                    <a href="{{ route('backend.admin.order.search',['paid' => true, 'cash_on_delivery' => true]) }}"
                        class="nav-link nav-toggle">
                         <i class="fa fa-fw fa-pie-chart"></i>
                         <span class="title">{{ trans('general.paid_online') }}</span>
