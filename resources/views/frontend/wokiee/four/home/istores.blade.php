3d33adc2600a71ec77084efbe16901c1baf52349
uusa35 <uusa35@gmail.com>
2022-01-06 19:11:56 +0300

    updates

--- resources/views/frontend/wokiee/four/home/istores.blade.php
+++ resources/views/frontend/wokiee/four/home/istores.blade.php
@@ -22,36 +22,7 @@
 
     @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.new_arrivals'), 'elements' => $latestProducts,'isGray' => true])
     @include('frontend.wokiee.four.partials._products_slider',['title' => trans('general.on_sale_products'), 'elements' => $onSaleProducts])
-    <div class="container-indent">
-        <div class="container">
-            <div class="row tt-layout-promo-box">
-                <div class="col-md-6">
-                    <a href="listing-left-column.html" class="tt-promo-box tt-one-child">
-                        <img src="images/loader.svg" data-src="images/promo/index04-promo-img-05.jpg" alt="">
-                        <div class="tt-description">
-                            <div class="tt-description-wrapper">
-                                <div class="tt-background"></div>
-                                <div class="tt-title-small">NEW IN:</div>
-                                <div class="tt-title-large">CLOTHING</div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-                <div class="col-md-6">
-                    <a href="listing-left-column.html" class="tt-promo-box tt-one-child">
-                        <img src="images/loader.svg" data-src="images/promo/index04-promo-img-06.jpg" alt="">
-                        <div class="tt-description">
-                            <div class="tt-description-wrapper">
-                                <div class="tt-background"></div>
-                                <div class="tt-title-small">CLEARANCE SALES</div>
-                                <div class="tt-title-large">GET UP TO <span class="tt-base-color">20% OFF</span></div>
-                            </div>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 {{--    @if(isset($categoriesHome) && $categoriesHome->isNotEmpty())--}}
 {{--        @include('frontend.wokiee.four.partials._five_categories',['elements' => $categoriesHome])--}}
 {{--    @endif--}}
--- resources/views/frontend/wokiee/four/partials/_products_user_show.blade.php
+++ resources/views/frontend/wokiee/four/partials/_products_user_show.blade.php
@@ -1,7 +1,7 @@
 @if(isset($products) && $products->isNotEmpty())
     <div class="row">
         @foreach($products as $product)
-            <div class="{{ count($products) < 2 ? 'col-lg-8' : 'col-lg-4' }} col-md-6 ">
+            <div class="{{ count($products) < 2 ? 'col-lg-8' : 'col-lg-4' }} col-md-6" style="margin-bottom: 15px;">
                 @include('frontend.wokiee.four.partials._product_widget',['element' => $product,'view' => true])
             </div>
         @endforeach
