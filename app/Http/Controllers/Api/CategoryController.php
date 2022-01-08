d9d407d167be307a7e363b1bd7bc7e0b7afe01e8
uusa35 <uusa35@gmail.com>
2022-01-08 16:53:17 +0300

    updates

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -60,7 +60,8 @@ class CategoryController extends Controller
         } elseif (request()->has('on_home') && request()->has('type')) {
             $elements = Category::active()->onHome()->where(request()->type, true)->get();
         } elseif (request()->has('is_market') && request()->is_market) {
-            $elements = Category::active()->onHome()->where('is_market', 1)->get();
+//            $elements = Category::active()->onHome()->where('is_market', 1)->get();
+            return [];
         }
         else {
             $elements = Category::active()->get();
