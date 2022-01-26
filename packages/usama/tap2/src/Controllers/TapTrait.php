5d2ce88277151bf8c5e8639d1508264507ad9cf5
uusa35 <uusa35@gmail.com>
2022-01-26 17:29:25 +0300

    updates

--- packages/usama/tap2/src/Controllers/TapTrait.php
+++ packages/usama/tap2/src/Controllers/TapTrait.php
@@ -21,7 +21,7 @@ trait TapTrait
         try {
             // 1- prepare data
             $order = Order::whereId($orderId)->with('order_metas','user')->first();
-            return response()->json(['message' => $order]);
+            return response()->json(['message' => $order->net_price]);
             // 2- update order with the reference_id
             // 3- return the paymentURL
             $curl = curl_init();
