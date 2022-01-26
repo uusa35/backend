06fb1bddfb4eeb89bfa7525565d27a1f8b9c7362
uusa35 <uusa35@gmail.com>
2022-01-26 17:03:41 +0300

    updates

--- packages/usama/tap2/src/Controllers/Tap2PaymentController.php
+++ packages/usama/tap2/src/Controllers/Tap2PaymentController.php
@@ -54,6 +54,7 @@ class Tap2PaymentController extends Controller
     {
         try {
             $order = $this->checkCart($request); // check cart then create order
+            return response()->json(['message' => 'here'], 200);
             if (is_string($order)) {
                 return response()->json(['message' => $order], 400);
             }
