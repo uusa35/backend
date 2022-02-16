517eb47cd6b1d375fb73c099553dc730715ab269
uusa35 <uusa35@gmail.com>
2022-02-16 16:32:03 +0300

    updates

--- resources/views/backend/partials/_sidebar_company.blade.php
+++ resources/views/backend/partials/_sidebar_company.blade.php
@@ -2,7 +2,7 @@
     <li class="nav-item {{ activeItem('product') }}">
         <a href="{{ route('backend.product.index')}}" class="nav-link nav-toggle">
             <i class="fa fa-fw fa-product-hunt"></i>
-            <span class="title">Products</span>
+            <span class="title">{{ trans('general.products') }}</span>
             <span class="selected"></span>
             <span class="arrow open"></span>
         </a>
@@ -10,35 +10,35 @@
             <li class="nav-item ">
                 <a href="{{ route('backend.product.index') }}" class="nav-link ">
                     <i class="fa fa-fw fa-product-hunt"></i>
-                    <span class="title">All Products</span>
+                    <span class="title">{{ trans('general.all_products') }}</span>
                     <span class="arrow"></span>
                 </a>
             </li>
             <li class="nav-item ">
                 <a href="{{ route('backend.product.index',['type' => 'active']) }}" class="nav-link ">
                     <i class="fa fa-fw fa-product-hunt"></i>
-                    <span class="title">Active Products</span>
+                    <span class="title">{{ trans('general.active') }} {{ trans('general.products') }}</span>
                     <span class="arrow"></span>
                 </a>
             </li>
             <li class="nav-item ">
                 <a href="{{ route('backend.product.index',['type' => 'on_sale']) }}" class="nav-link ">
                     <i class="fa fa-fw fa-percent"></i>
-                    <span class="title">On Sale Products</span>
+                    <span class="title">{{ trans('general.on_sale_products') }}</span>
                     <span class="arrow"></span>
                 </a>
             </li>
             <li class="nav-item ">
                 <a href="{{ route('backend.product.create') }}" class="nav-link ">
                     <i class="fa fa-fw fa-plus-square"></i>
-                    <span class="title">Create New Product</span>
+                    <span class="title">{{ trans('general.new_product') }}</span>
                     <span class="arrow"></span>
                 </a>
             </li>
             <li class="nav-item ">
                 <a href="{{ route('backend.product.trashed') }}" class="nav-link ">
                     <i class="fa fa-fw fa-recycle"></i>
-                    <span class="title">Trashed</span>
+                    <span class="title">{{ trans('general.trashed') }}</span>
                     <span class="arrow"></span>
                 </a>
             </li>
