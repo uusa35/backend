06824fc65e2c5baacfec0ee34a7c5455876c21a5
uusa35 <uusa35@gmail.com>
2022-03-20 19:35:26 +0300

    updates

--- resources/views/frontend/wokiee/four/partials/footer-istores.blade.php
+++ resources/views/frontend/wokiee/four/partials/footer-istores.blade.php
@@ -33,10 +33,85 @@
             </div>
         </div>
     @endif
+
     <div class="tt-footer-col ">
         <div class="container">
             <div class="row">
+                {{--                     contact us--}}
+                <div class="col-md-6 col-lg-2 col-xl-3">
+                    <div class="tt-newsletter">
+                        <div class="tt-mobile-collapse">
+                            <h4 class="tt-collapse-title">
+                                {{ trans('general.contact_us_info') }}
+                            </h4>
+                            <div class="tt-collapse-content">
+                                <address>
+                                    @if($settings->address)
+                                        <p><i class="fa fa-fw fa-map-marker"></i> {{ $settings->address }}</p>
+                                    @endif
+                                    @if($settings->phone)
+                                        <p><i class="fa fa-fw fa-phone"></i> {{ $settings->phone }}</p>
+                                    @endif
+                                    @if($settings->mobile)
+                                        <p><i class="fa fa-fw fa-mobile"></i> {{ $settings->mobile }}</p>
+                                    @endif
+                                    @if($settings->instagram && env('EVENTKM'))
+                                        <p><i class="fa fa-fw fa-instagram"></i><a
+                                                href="{{ $settings->instagram }}"> {{ $settings->company }}</a>
+                                        </p>
+                                    @endif
+                                    @if($settings->whatsapp)
+                                        <p><i class="fa fa-fw fa-whatsapp"></i> <a
+                                                href="https://api.whatsapp.com/send?phone={{ $settings->whatsapp }}&text={{ env('APP_NAME') }}">{{ $settings->whatsapp }}</a>
+                                        </p>
+                                    @endif
+                                    @if($settings->latitude && $settings->longitude)
+                                        <p><i class="fa fa-fw fa-location-arrow"></i> <a
+                                                href="https://www.google.com/maps/search/?api=1&query={{ $settings->latitude  }},{{ $settings->longitude }}">
+                                                {{ trans('general.location') }}
+                                            </a></p>
+                                    @endif
+                                    @if($settings->email)
+                                        <p><span>{{ trans('general.mail') }}:</span> <a
+                                                href="mailto:{{ $settings->email }}">{{ $settings->email }}</a>
+                                        </p>
+                                        <p>
+                                            @endif
+                                            @if(!str_contains(trans('message.duty_time_message'),'message.'))
+                                                <span>{{ trans('general.duty_time') }}:</span> {!! trans('message.duty_time_message')  !!}
+                                            @endif
+                                        </p>
+                                </address>
+                                @if($settings->apple || $settings->android)
+                                    <ul class="tt-list pt-2">
+                                        <h4 class="tt-collapse-title">
+                                            {{ trans('general.find_us_on_stores') }}
+                                        </h4>
+                                        @if($settings->apple)
+                                            <li>
+                                                <a href="{{ url($settings->apple) }}">
+                                                    <img src="{{ asset('images/apple.png') }}"
+                                                         alt="{{ $settings->company }}"
+                                                         class="img-responsive" style="max-width: 150px;">
+                                                </a>
+                                            </li>
+                                        @endif
+                                        @if($settings->android)
+                                            <li>
+                                                <a href="{{ url($settings->android) }}">
+                                                    <img src="{{ asset('images/android.png') }}"
+                                                         alt="{{ $settings->company }}"
+                                                         class="img-responsive" style="max-width: 150px;">
+                                                </a>
+                                            </li>
+                                        @endif
+                                    </ul>
+                                @endif
+                            </div>
+                        </div>
+                    </div>
 
+                </div>
                 {{--                 aboutus --}}
                 @if(trans()->has('message.footer_about_us'))
                     <div class="col-md-6 col-lg-2 col-xl-3">
@@ -155,81 +230,6 @@
                             </ul>
                         </div>
                     </div>
-                </div>
-                {{--                     contact us--}}
-                <div class="col-md-6 col-lg-2 col-xl-3">
-                    <div class="tt-newsletter">
-                        <div class="tt-mobile-collapse">
-                            <h4 class="tt-collapse-title">
-                                {{ trans('general.contact_us_info') }}
-                            </h4>
-                            <div class="tt-collapse-content">
-                                <address>
-                                    @if($settings->address)
-                                        <p><i class="fa fa-fw fa-map-marker"></i> {{ $settings->address }}</p>
-                                    @endif
-                                    @if($settings->phone)
-                                        <p><i class="fa fa-fw fa-phone"></i> {{ $settings->phone }}</p>
-                                    @endif
-                                    @if($settings->mobile)
-                                        <p><i class="fa fa-fw fa-mobile"></i> {{ $settings->mobile }}</p>
-                                    @endif
-                                    @if($settings->instagram && env('EVENTKM'))
-                                        <p><i class="fa fa-fw fa-instagram"></i><a
-                                                href="{{ $settings->instagram }}"> {{ $settings->company }}</a>
-                                        </p>
-                                    @endif
-                                    @if($settings->whatsapp)
-                                        <p><i class="fa fa-fw fa-whatsapp"></i> <a
-                                                href="https://api.whatsapp.com/send?phone={{ $settings->whatsapp }}&text={{ env('APP_NAME') }}">{{ $settings->whatsapp }}</a>
-                                        </p>
-                                    @endif
-                                    @if($settings->latitude && $settings->longitude)
-                                        <p><i class="fa fa-fw fa-location-arrow"></i> <a
-                                                href="https://www.google.com/maps/search/?api=1&query={{ $settings->latitude  }},{{ $settings->longitude }}">
-                                                {{ trans('general.location') }}
-                                            </a></p>
-                                    @endif
-                                    @if($settings->email)
-                                        <p><span>{{ trans('general.mail') }}:</span> <a
-                                                href="mailto:{{ $settings->email }}">{{ $settings->email }}</a>
-                                        </p>
-                                        <p>
-                                            @endif
-                                            @if(!str_contains(trans('message.duty_time_message'),'message.'))
-                                                <span>{{ trans('general.duty_time') }}:</span> {!! trans('message.duty_time_message')  !!}
-                                            @endif
-                                        </p>
-                                </address>
-                                @if($settings->apple || $settings->android)
-                                    <ul class="tt-list pt-2">
-                                        <h4 class="tt-collapse-title">
-                                            {{ trans('general.find_us_on_stores') }}
-                                        </h4>
-                                        @if($settings->apple)
-                                            <li>
-                                                <a href="{{ url($settings->apple) }}">
-                                                    <img src="{{ asset('images/apple.png') }}"
-                                                         alt="{{ $settings->company }}"
-                                                         class="img-responsive" style="max-width: 150px;">
-                                                </a>
-                                            </li>
-                                        @endif
-                                        @if($settings->android)
-                                            <li>
-                                                <a href="{{ url($settings->android) }}">
-                                                    <img src="{{ asset('images/android.png') }}"
-                                                         alt="{{ $settings->company }}"
-                                                         class="img-responsive" style="max-width: 150px;">
-                                                </a>
-                                            </li>
-                                        @endif
-                                    </ul>
-                                @endif
-                            </div>
-                        </div>
-                    </div>
-
                 </div>
             </div>
         </div>
