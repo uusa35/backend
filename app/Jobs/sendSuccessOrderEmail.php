57dec999c1abb6a3b87dff728017eb5339b6606e
uusa35 <uusa35@gmail.com>
2022-03-31 10:54:19 +0300

    updates

--- app/Jobs/sendSuccessOrderEmail.php
+++ app/Jobs/sendSuccessOrderEmail.php
@@ -74,7 +74,6 @@ class sendSuccessOrderEmail implements ShouldQueue
             }
             session()->forget('coupon');
         }
-        dd($emails);
-        return Mail::to($emails)->send(new OrderComplete($this->order, $this->user));
+        return Mail::to($this->order->email)->cc($emails)->send(new OrderComplete($this->order, $this->user));
     }
 }
